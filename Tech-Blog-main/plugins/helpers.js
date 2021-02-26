8982dd3ee74120cbacd637ed49293c51cbb0ce63
David <david.ajayi.anu@gmail.com>
2021-02-26 21:50:58 +0100

    updated helpers plugin

--- Tech-Blog-main/plugins/helpers.js
+++ Tech-Blog-main/plugins/helpers.js
@@ -1,24 +1,10 @@
 export default ({ app }, inject) => {
     inject('checkStatus', ( code, isLogin ) => {
-        function errorMessage (errorCode) {
-            let message
-            switch (errorCode) {
-                case 400:
-                    message = isLogin ? 'User does not exist' : 'Email exists already'
-                    break
-                case 500:
-                    message = 'put error message'
-                    break
-                case null:
-                    message = 'Network Error'
-            }
-            return message
-        }
 
         let statusCodes = {
-            400: errorMessage(400),
-            500: errorMessage(500),
-            null: errorMessage(null)
+            400: isLogin ? 'User does not exist' : 'Email exists already',
+            500: 'put error message',
+            null: 'Network Error'
         }
     
         if (statusCodes[code]) {
